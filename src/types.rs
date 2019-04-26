d09a8f2a859d1239dbcaaa71bcf6b3eb4de18921
nsmryan <nsmryan@gmail.com>
2019-04-26 07:59:00 -0400

    Add types.rs

---
+++ src/types.rs
@@ -0,0 +1,25 @@
+use std::ops::{BitOrAssign, Sub, BitAnd, BitOr};
+use std::fmt::Debug;
+
+use bitstream_io::*;
+
+use num::Zero;
+
+
+/* BitWise trait for Primitive Types */
+// note that this does not require One as the function one() is provided by
+// the Numeric type from bitstream_io.
+pub trait BitWise: BitOrAssign<Self> + Sub<Self, Output = Self> +
+                   BitAnd<Output = Self> + BitOr<Output = Self> +
+                   Zero + Numeric {}
+
+impl BitWise for u8  {}
+impl BitWise for u16 {}
+impl BitWise for u32 {}
+impl BitWise for u64 {}
+
+impl BitWise for i8  {}
+impl BitWise for i16 {}
+impl BitWise for i32 {}
+impl BitWise for i64 {}
+
